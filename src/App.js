70f3b2543c814dbdee3e1de1398d81c8e5ac8a37
gregoriusjimmy <gregoriusjimmy@gmail.com>
2020-10-10 19:18:59 +0700

    finishing filter by date and style

--- src/App.js
+++ src/App.js
@@ -34,6 +34,37 @@ class App extends React.Component {
     this.setState({ searchByName: e.target.value });
   };
 
+  filterProducts = () => {
+    const {
+      products,
+      searchByName,
+      searchByFurnitureStyles,
+      searchByTimeDelivery,
+    } = this.state;
+    let styles = searchByFurnitureStyles;
+    let convertedTimeDelivery = searchByTimeDelivery.length ? 0 : 111;
+    searchByTimeDelivery.forEach((deliveryTime) => {
+      if (deliveryTime === '1 week') {
+        convertedTimeDelivery += 7;
+      } else if (deliveryTime === '2 weeks') {
+        convertedTimeDelivery += 14;
+      } else if (deliveryTime === '1 month') {
+        convertedTimeDelivery += 30;
+      } else {
+        convertedTimeDelivery += 60;
+      }
+    });
+
+    const displayedProduct = products.filter((product) => {
+      return (
+        product.name.toLowerCase().includes(searchByName.toLowerCase()) &&
+        styles.every((style) => product.furniture_style.includes(style)) &&
+        product.delivery_time <= convertedTimeDelivery
+      );
+    });
+
+    return displayedProduct;
+  };
   handleChangeCheckboxStyle = (e) => {
     let currentStyles = this.state.searchByFurnitureStyles;
     if (e.target.checked) {
@@ -43,21 +74,18 @@ class App extends React.Component {
     }
     this.setState({ searchByFurnitureStyles: currentStyles });
   };
-  filterProducts = () => {
-    const { products, searchByName, searchByFurnitureStyles } = this.state;
-    let displayedProduct = [];
-    if (searchByFurnitureStyles.length) {
-      displayedProduct = products.filter((product) => {
-        return searchByFurnitureStyles.every((style) => console.log(style));
-      });
+  handleChangeCheckboxDeliveryTime = (e) => {
+    let currentDeliveryTime = this.state.searchByTimeDelivery;
+    if (e.target.checked) {
+      currentDeliveryTime.push(e.target.value);
+    } else {
+      currentDeliveryTime = currentDeliveryTime.filter(
+        (deliveryTime) => deliveryTime !== e.target.value
+      );
     }
-    displayedProduct = products.filter((product) => {
-      return product.name.toLowerCase().includes(searchByName.toLowerCase());
-    });
-    return displayedProduct;
+    this.setState({ searchByTimeDelivery: currentDeliveryTime });
   };
 
-  handleChangeCheckboxDeliveryTime = (e) => {};
   render() {
     const { searchByName } = this.state;
     const filteredProducts = this.filterProducts();
