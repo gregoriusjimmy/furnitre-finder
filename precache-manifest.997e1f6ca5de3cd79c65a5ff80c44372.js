29f08f59a3409880fe4f805c687092fe1f3d7988
gregoriusjimmy <gregoriusjimmy@gmail.com>
2020-10-10 19:22:17 +0700

    Updates

---
+++ precache-manifest.997e1f6ca5de3cd79c65a5ff80c44372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3dbf8f98f57c4c30a84e98279c17ce",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "d839a5592a75fbf9e790",
+    "url": "/furniture-finder/static/css/main.a5df4da5.chunk.css"
+  },
+  {
+    "revision": "06d212aee879d791267d",
+    "url": "/furniture-finder/static/js/2.ede58e6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/furniture-finder/static/js/2.ede58e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d839a5592a75fbf9e790",
+    "url": "/furniture-finder/static/js/main.f62decb3.chunk.js"
+  },
+  {
+    "revision": "9df1d94304e174b439fb",
+    "url": "/furniture-finder/static/js/runtime-main.fa46ef05.js"
+  }
+]);
